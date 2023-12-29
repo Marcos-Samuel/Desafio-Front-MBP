e815f48d0d31d8798824b41fa308d9e84ce1a891
Marcos-Samuel <marcos_samueloff@outlook.com>
2023-12-29 12:32:59 -0300

    <BUILD>[Iniciando Projeto]: <Iniciando Projeto em Vite>

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+    'white-100': '#fff',
+    'white-200': '#ECEDEE',
+    
+    'background-color': '#0F0F0F',
+    
+    'pink-100': '#CC4090',
+    
+    'blue-100': '#05A2C2',
+    
+    'orage-100': '#CC4090',
+    
+    'purple-100': '#9B19C2',
+  
+    'green-100': '#00B37E',
+    
+    'yellow-100': '#E9FF1A',
+   
+    'red-100': '#FF5964',
+    
+    'gray-100': '#424242',
+  } as const
